6d6d11c4dd65699d1aff4bc36f6a053c0196d7cb
Tim Kostreva <tkostreva@gmail.com>
2012-04-04 11:48:14 -0400

    Commiting .h file

---
+++ robot_vision.h
@@ -0,0 +1,24 @@
+#ifndef _robot_vision_
+#define _robot_vision_
+
+#include "robot_if.h"
+#include "robot_color.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define area_threshold 0.75
+#define multiplier 500
+
+typedef enum _square_state_ {
+	hasTwoPair,
+	hasOnePair,
+	twoLargest,
+	onlyLargest,
+	noneFound
+} square_state;
+
+
+void center_robot(robot_if_t *ri, IplImage *image, IplImage *final_threshold, char *bot_name);
+
+#endif
